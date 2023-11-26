3e0482d7722d0706c55df1609403532fdffd2a52
Jyothis Joseph <86906521+jyopzz@users.noreply.github.com>
2023-11-26 15:38:24 +0530

    Update settings.py

--- sampletestdjango/sampletestdjango/settings.py
+++ sampletestdjango/sampletestdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pgy@2vb&g5y6jdj9)xu$m*zp74ij@f0l$))41yyd@n#ch4$yle'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['35.171.195.232','localhost']
 
