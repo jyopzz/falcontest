1f2e315e11d998ab289a6f6f8606afc42b29413f
Jyothis Joseph <86906521+jyopzz@users.noreply.github.com>
2023-11-27 10:51:13 +0530

    Update settings.py

--- sampletestdjango/sampletestdjango/settings.py
+++ sampletestdjango/sampletestdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pgy@2vb&g5y6jdj9)xu$m*zp74ij@f0l$))41yyd@n#ch4$yle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['15.207.22.174','localhost']
+ALLOWED_HOSTS = ['34.236.23.195','localhost']
 
 
 # Application definition
